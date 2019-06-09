87082401586444d6d8a5617d8bf4b7297c9e0512
Larry850806 <pudding850806@gmail.com>
2019-06-09 14:11:10 +0800

    Finish

--- test.sh
+++
@@ -1,3 +0,0 @@
-./user_program/master ./data/file$1_in $2 & 
-./user_program/slave ./data/file$1_out $3 127.0.0.1
-dmesg | tail -n 8
--- user_program/master.c
+++ user_program/master.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -9,10 +11,19 @@
 #include <sys/types.h>
 #include <sys/time.h>
 
-#define PAGE_SIZE 4096
 #define BUF_SIZE 512
-size_t get_filesize(const char* filename);//get the size of the input file
+#define PAGE_SIZE 4096
+#define MAP_SIZE PAGE_SIZE * 50
 
+#define master_IOCTL_CREATESOCK 0x12345677
+#define master_IOCTL_MMAP 0x12345678
+#define master_IOCTL_EXIT 0x12345679
+
+size_t size_of(const char* filename){ // get the size of the input file
+    struct stat st;
+    stat(filename, &st);
+    return st.st_size;
+}
 
 int main (int argc, char* argv[])
 {
@@ -29,62 +40,79 @@ int main (int argc, char* argv[])
 	strcpy(file_name, argv[1]);
 	strcpy(method, argv[2]);
 
+	if( (dev_fd = open("/dev/master_device", O_RDWR)) < 0){
+            perror("failed to open /dev/master_device\n");
+            exit(1);
+    }
 
-	if( (dev_fd = open("/dev/master_device", O_RDWR)) < 0)
-	{
-		perror("failed to open /dev/master_device\n");
-		return 1;
-	}
 	gettimeofday(&start ,NULL);
-	if( (file_fd = open (file_name, O_RDWR)) < 0 )
-	{
-		perror("failed to open input file\n");
-		return 1;
+
+	if( (file_fd = open (file_name, O_RDWR)) < 0 ){
+            perror("failed to open input file\n");
+            exit(1);
+    }
+
+	if( (file_size = size_of(file_name)) < 0){
+            perror("failed to get filesize\n");
+            exit(1);
+    }
+
+
+	if(ioctl(dev_fd, master_IOCTL_CREATESOCK) == -1){
+            // create socket and accept the connection from the slave
+            perror("ioclt server create socket error\n");
+            exit(1);
+    }
+
+
+	switch(method[0]){
+        case 'f': //fcntl : read()/write()
+            do{
+                ret = read(file_fd, buf, sizeof(buf)); // read from the input file
+                write(dev_fd, buf, ret);//write to the the device
+            }while(ret > 0);
+            break;
+        case 'm':
+            while (offset < file_size) {
+                size_t map_length = MAP_SIZE;
+                if (file_size < map_length + offset) {
+                    map_length = file_size - offset;
+                }
+
+                // do mmap
+                file_address = mmap(NULL, map_length, PROT_READ, MAP_SHARED, file_fd, offset);
+                kernel_address = mmap(NULL, map_length, PROT_WRITE, MAP_SHARED, dev_fd, 0);
+
+                // copy memory
+                memcpy(kernel_address, file_address, map_length);
+
+                offset += map_length;
+                ioctl(dev_fd, master_IOCTL_MMAP, map_length);
+                int cnt;
+                for(cnt = 0; PAGE_SIZE * cnt < map_length; cnt++)
+                    ioctl(dev_fd, 0, file_address + PAGE_SIZE * cnt);
+
+                // release mmap
+                munmap(file_address, map_length);
+                munmap(kernel_address, map_length);
+            }
+            break;
 	}
 
-	if( (file_size = get_filesize(file_name)) < 0)
-	{
-		perror("failed to get filesize\n");
-		return 1;
-	}
+    bool isFinished = ioctl(dev_fd, master_IOCTL_EXIT) == -1;
+	if(isFinished){
+        // end sending data, close the connection
+        perror("ioclt server exits error\n");
+        exit(1);
+    }
 
-
-	if(ioctl(dev_fd, 0x12345677) == -1) //0x12345677 : create socket and accept the connection from the slave
-	{
-		perror("ioclt server create socket error\n");
-		return 1;
-	}
-
-
-	switch(method[0])
-	{
-		case 'f': //fcntl : read()/write()
-			do
-			{
-				ret = read(file_fd, buf, sizeof(buf)); // read from the input file
-				write(dev_fd, buf, ret);//write to the the device
-			}while(ret > 0);
-			break;
-	}
-
-	if(ioctl(dev_fd, 0x12345679) == -1) // end sending data, close the connection
-	{
-		perror("ioclt server exits error\n");
-		return 1;
-	}
 	gettimeofday(&end, NULL);
-	trans_time = (end.tv_sec - start.tv_sec)*1000 + (end.tv_usec - start.tv_usec)*0.0001;
-	printf("Transmission time: %lf ms, File size: %d bytes\n", trans_time, file_size / 8);
+
+        trans_time = (end.tv_sec - start.tv_sec)*1000 + (end.tv_usec - start.tv_usec)*0.0001;
+	printf("Transmission time: %lf ms, File size: %d bytes\n", trans_time, (int)file_size / 8);
 
 	close(file_fd);
 	close(dev_fd);
-
 	return 0;
 }
 
-size_t get_filesize(const char* filename)
-{
-    struct stat st;
-    stat(filename, &st);
-    return st.st_size;
-}
